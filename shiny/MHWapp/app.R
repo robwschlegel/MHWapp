68c63353c4f871f19fa53a563164eef08a197272
Robert William Schlegel <robwschlegel@gmail.com>
2018-10-12 14:29:24 -0230

    Basic product up and running

--- shiny/MHWapp/app.R
+++ shiny/MHWapp/app.R
@@ -2,32 +2,32 @@
 # Load libraries ----------------------------------------------------------
 
 library(tidyverse)
+library(shiny)
 library(leaflet)
 library(raster)
 library(rgdal)
-library(htmlwidgets)
-library(mapview)
+# library(htmlwidgets)
+# library(mapview)
 library(RColorBrewer)
 
 
 # Load data ---------------------------------------------------------------
 
+# The event clims
 # load("data/MHW_cat.RData")
-load("../../data/MHW_cat.RData")
+# MHW_cat_clim <- MHW_cat %>%
+#   unnest(cat) %>%
+#   filter(row_number() %% 2 == 1) %>%
+#   unnest(cat) %>%
+#   mutate(category = factor(category, levels = c("I Moderate", "II Strong",
+#                                                 "III Severe", "IV Extreme")),
+#          intensity = round(intensity, 2)) %>%
+#   dplyr::select(lon, lat, t, temp, intensity, category)
+# save(MHW_cat_clim, file = "shiny/MHWapp/MHW_cat_clim.RData")
 
 # The event categories
-# MHW_cat_event <- MHW_cat %>% 
-#   unnest(cat) %>% 
-#   filter(row_number() %% 2 == 0) %>% 
-#   unnest(cat)
-
-# The event clims
-MHW_cat_clim <- MHW_cat %>% 
-  unnest(cat) %>% 
-  filter(row_number() %% 2 == 1) %>% 
-  unnest(cat) %>% 
-  mutate(category = factor(category, levels = c("I Moderate", "II Strong", 
-                                                "III Severe", "IV Extreme")))
+# load("shiny/MHWapp/MHW_cat_clim.RData")
+load("MHW_cat_clim.RData")
 
 # The category colour pallette
 fillColCat <- c(
@@ -41,111 +41,102 @@ fillColCat <- c(
 # Base map ----------------------------------------------------------------
 
 # start basemap
-map <- leaflet() %>% 
-  
-  # add ocean basemap
-  addProviderTiles(providers$Esri.OceanBasemap) %>%
-  
-  # add another layer with place names
-  addProviderTiles(providers$Hydda.RoadsAndLabels, group = 'Place names') %>%
-  
-  # add graticules from a NOAA webserver
-  addWMSTiles(
-    "https://gis.ngdc.noaa.gov/arcgis/services/graticule/MapServer/WMSServer/",
-    layers = c("1-degree grid", "5-degree grid"),
-    options = WMSTileOptions(format = "image/png8", transparent = TRUE),
-    attribution = NULL,group = 'Graticules') %>%
-  
-  # focus map in a certain area / zoom level
-  setView(lng = 25, lat = -35, zoom = 5) %>%
-  
-  # add layers control
-  # addLayersControl(overlayGroups = c('Place names',
-  #                                    'Graticules',
-  #                                    'Points',
-  #                                    'Lines',
-  #                                    'Polygons'),
-  #                  options = layersControlOptions(collapsed = FALSE),
-  #                  position = 'topright') %>%
-  
-  # list groups to hide on startup
-hideGroup(c('Place names'))
-
-val <- c(1, 2)
-pal <-  colorFactor(c("#ffc866", "#ff6900"), levels = val,
-                    na.color = "transparent", ordered = T)
+# map <- leaflet() %>% 
+#   # add ocean basemap
+#   addProviderTiles(providers$Esri.OceanBasemap) %>%
+#   # add another layer with place names
+#   addProviderTiles(providers$Hydda.RoadsAndLabels, group = 'Place names') %>%
+#   # add graticules from a NOAA webserver
+#   addWMSTiles(
+#     "https://gis.ngdc.noaa.gov/arcgis/services/graticule/MapServer/WMSServer/",
+#     layers = c("1-degree grid", "5-degree grid"),
+#     options = WMSTileOptions(format = "image/png8", transparent = TRUE),
+#     attribution = NULL,group = 'Graticules') %>%
+#   # focus map in a certain area / zoom level
+#   setView(lng = 25, lat = -35, zoom = 5) %>%
+#   hideGroup(c('Place names'))
+# 
+# val <- c(1, 2)
+# pal <-  colorFactor(c("#ffc866", "#ff6900"), levels = val,
+#                     na.color = "transparent", ordered = T)
 
 
 # UI ----------------------------------------------------------------------
 
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   # titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a calendar input for dates
-   pageWithSidebar(
-     headerPanel('Map'),
-     sidebarPanel(
-       dateInput(inputId = "dates",
-                 label = "Choose a day to visualise", 
-                 value = MHW_cat_clim$t[MHW_cat_clim$intensity == max(MHW_cat_clim$intensity, na.rm = T)][1],
-                 # start = min(MHW_cat_clim$t),
-                 # end = max(MHW_cat_clim$t),
-                 format = "yyyy-mm-dd",
-                 startview = "month")
-     ),
-     # mainPanel(uiOutput('MHW_map')
-     leafletOutput("MHW_map")
-     )
-      
-      # # Show a plot of the generated distribution
-      # mainPanel(
-      #    plotOutput("MHW_map")
-      # )
-   )
-# )
+# Define UI for map controls
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(top = 10, right = 10,
+                # sliderInput("range", "Magnitudes", min(quakes$mag), max(quakes$mag),
+                #             value = range(quakes$mag), step = 0.1
+                # ),
+                selectInput("colors", "Color Scheme",
+                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                ),
+                dateInput("date_choice", "Date", 
+                          value = MHW_cat_clim$t[MHW_cat_clim$intensity == max(MHW_cat_clim$intensity)][1], 
+                          min = min(MHW_cat_clim$t), max = max(MHW_cat_clim$t)
+                ),
+                checkboxInput("legend", "Show legend", TRUE)
+  )
+)
 
 
 # Server ------------------------------------------------------------------
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+# Define server logic
+server <- function(input, output, session) {
   
-  MHW_sub <- reactive({
-    MHW_sub <- MHW_cat_clim
-    # Subset date
-    date_choice <- input$dates
-    MHW_sub <- MHW_sub[MHW_sub$t == date_choice,]
-    MHW_sub <- MHW_sub[,c(1,2,5)]
-    colnames(MHW_sub) <- c("X","Y","Z")
-    MHW_sub$Z <- as.numeric(MHW_sub$Z)
-    MHW_sub <- rasterFromXYZ(MHW_sub, res = c(0.25, 0.25), digits = 2, 
-                             crs = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") 
-  })
-
-  output$MHW_map <- renderLeaflet({
-    map %>% 
-      # addRasterImage(x = MHW_sub, colors = pal, opacity = 0.5, method = "ngb") %>%
-      # leaflet::
-      # addCircleMarkers(data = MHW_sub$MHW_sub, ~lon, ~lat,
-      #                  weight = 1.0,
-      #                  color = "grey",
-      #                  fillColor = "white",
-      #                  radius = 10,
-      #                  fillOpacity = 0.0,
-      #                  stroke = T,
-      #                  label = ~paste0('Event at: ',
-      #                                  as.character(round(lat,3)), ', ',
-      #                                  as.character(round(lon,3))),
-      #                  group = 'Points') %>%
-      addScaleBar() #%>%
-      # tileOptions(minZoom = 10) %>% 
-      # addLegend(pal = pal, values = val, title = "MHW Category", labels = c("I Moderate", "II Strong"))
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    MHW_cat_clim[MHW_cat_clim$t == input$date_choice,]
   })
   
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(input$colors, MHW_cat_clim$intensity)
+  })
+  
+  output$map <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet(MHW_cat_clim) %>% addTiles() %>%
+      fitBounds(~min(lon), ~min(lat), ~max(lon), ~max(lat))
+  })
+  
+  # Incremental changes to the map (in this case, replacing the
+  # circles when a new color is chosen) should be performed in
+  # an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  observe({
+    pal <- colorpal()
+    
+    leafletProxy("map", data = filteredData()) %>%
+      clearShapes() %>%
+      addCircles(radius = ~20^intensity, weight = 1, color = "#777777",
+                 fillColor = ~pal(intensity), fillOpacity = 0.7, 
+                 popup = ~paste0("Daily intensity: ", round(intensity, 2))
+      )
+  })
+  
+  # Use a separate observer to recreate the legend as needed.
+  observe({
+    proxy <- leafletProxy("map", data = MHW_cat_clim)
+    
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearControls()
+    if (input$legend) {
+      pal <- colorpal()
+      proxy %>% addLegend(position = "bottomright",
+                          pal = pal, values = ~intensity
+      )
+    }
+  })
 }
 
 
