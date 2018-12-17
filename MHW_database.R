f3cdfa7829c8858a47e1eb96cae5e1e5c3a480c1
Robert W Schlegel <robwschlegel@gmail.com>
2018-12-17 18:31:45 +0000

    Getting ready for databases

---
+++ MHW_database.R
@@ -0,0 +1,15 @@
+# The purpose of this script is to provide the space in which the 
+# database used by the app is created
+
+
+# Libraries ---------------------------------------------------------------
+
+library(tidyverse)
+library(dbplyr)
+library(DBI)
+library(RSQLite)
+
+
+# Data --------------------------------------------------------------------
+
+
