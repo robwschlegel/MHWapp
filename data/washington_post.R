104630057000f56571d3207648a9c4c4c59bdcde
robwschlegel <robwschlegel@gmail.com>
2019-08-29 15:27:11 -0300

    Created an animation of the Uruguay MHW

--- data/washington_post.R
+++ data/washington_post.R
@@ -9,6 +9,8 @@
 
 library(tidyverse)
 library(raster)
+library(gganimate, lib.loc = "~/R-packages/")
+doMC::registerDoMC(cores = 50)
 
 
 # Load data ---------------------------------------------------------------
@@ -18,7 +20,7 @@ readRDS_date <- function(file_name){
   file_date <- sapply(strsplit(file_name, "/"), "[[", 4)
   file_date <- sapply(strsplit(file_date, "[.]"), "[[", 3)
   base <- readRDS(file_name) %>% 
-    mutate(t = file_date) %>% 
+    mutate(t = as.Date(file_date)) %>% 
     dplyr::select(t, lon, lat, category)
   return(base)
 }
@@ -56,16 +58,62 @@ raster_MHW <- function(df){
   df_raster <- raster::rasterFromXYZ(df, res = c(0.25, 0.25), digits = 3, crs = inputProj)
 }
 
-# Create raster
+# Create single raster
 MHW_2017_single_raster <- raster_MHW(MHW_2017_single)
+
+# Save single raster
+writeRaster(MHW_2017_single_raster, "data/washington_post.asc")
 
 
 # Visualise ---------------------------------------------------------------
 
+# Plot a single raster file
 plot(MHW_2017_single_raster)
 
+# Uruguay bounding box
+bbox_uruguay <- c(-67, -6, -55, -23)
 
-# Save --------------------------------------------------------------------
+# Test figure to look at extent of focus MHW
+ggplot(data = filter(MHW_2017, t == "2017-02-25"), aes(x = lon, y = lat)) +
+  borders(fill = "grey70", colour = "black") +
+  geom_raster(aes(fill = category)) +
+  scale_fill_manual("Category",
+                    values = c("#ffc866", "#ff6900", "#9e0000", "#2d0000"),
+                    labels = c("I Moderate", "II Strong", "III Severe", "IV Extreme")) +
+  coord_cartesian(xlim = c(bbox_uruguay[1], bbox_uruguay[2]),
+                  ylim = c(bbox_uruguay[3], bbox_uruguay[4])) +
+  labs(x = NULL, y = NULL)
 
-writeRaster(MHW_2017_single_raster, "data/washington_post.asc")
+
+# Animate -----------------------------------------------------------------
+
+# The base map
+world <- ggplot() +
+  borders(fill = "grey70", colour = "black") +
+  coord_cartesian(xlim = c(bbox_uruguay[1], bbox_uruguay[2]),
+                  ylim = c(bbox_uruguay[3], bbox_uruguay[4])) +
+  labs(x = NULL, y = NULL)+
+  theme(panel.background = element_rect(fill = "lightblue"))
+
+# The number of day steps for animation
+anim_days <- length(unique(MHW_2017$t))
+
+cat_plot <- world +
+  geom_raster(data = MHW_2017, aes(x = lon, y = lat, fill = category)) +
+  scale_fill_manual("Category",
+                    values = c("#ffc866", "#ff6900", "#9e0000", "#2d0000"),
+                    labels = c("I Moderate", "II Strong", "III Severe", "IV Extreme")) +
+  labs(title = 'Date: {frame_time}') +
+  transition_time(time = t)# +
+  # ease_aes("linear", interval = 2)
+
+# It seems as though it is impossible to get the video to render as anything other than 4 seconds...
+cat_anim <- animate(plot = cat_plot, renderer = ffmpeg_renderer())
+# cat_anim <-  animate(cat_anim, fps = 10, duration = anim_days)
+
+# Save the animation
+anim_save(animation = cat_anim, filename = "washington_post.mp4", path = "anim")
+
+# Create a slower video using console commands
+system("ffmpeg -i anim/washington_post.mp4 -vf 'setpts=7*PTS' anim/washington_post_slow.mp4")
 
