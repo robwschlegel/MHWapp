fa3c4d8073f382acae6aa7687a9c8a244fce0c5e
robwschlegel <robwschlegel@gmail.com>
2020-05-04 17:48:54 -0300

    Was playing around with summary display

--- shiny/modules/summary-server.R
+++ shiny/modules/summary-server.R
@@ -1,16 +1,16 @@
 summary <- function(input, output, session) {
   ns <- session$ns
-
-
+  
+  
 # Reactive UI -------------------------------------------------------------
-
+  
   output$summary_UI <- renderUI({
     if(input$summary_tabs == "annual_tab"){
       sidebarPanel(width = 2,
                    selectInput(inputId = ns("summary_year"), 
                                label = h4("Annual summary"),
                                choices = seq(1982, lubridate::year(Sys.time())), 
-                                             selected = lubridate::year(Sys.time())-1, multiple = F),
+                               selected = lubridate::year(Sys.time())-1, multiple = F),
                    # h4("Caption:"),
                    dropdownButton(
                      p("Annual global marine heatwave (MHW) occurrence."),
@@ -26,36 +26,36 @@ summary <- function(input, output, session) {
                      label = "Caption", tooltip = FALSE),
                    br(),
                    downloadButton(outputId = ns("annual_summary_dl"))
-                   )
+      )
     } else if(input$summary_tabs == "historic_tab"){
       sidebarPanel(width = 2,
                    h4("Historic summary"),
                    shinyWidgets::dropdownButton(
-                    p("Historic global marine heatwave (MHW) occurrence. MHWs calculated with the Hobday et al. 2016 definition and categories calculated
+                     p("Historic global marine heatwave (MHW) occurrence. MHWs calculated with the Hobday et al. 2016 definition and categories calculated
                       with the Hobday et al. 2018 deifnition."),
-                    p("A) The daily average percent of the ocean that experienced a MHW."),
-                    p("B) The proportion of the highest category of MHW experienced."),
-                    p("C) The total average of daily MHW occurrence throughout the entire ocean."),
-                    circle = FALSE,
-                    right = FALSE, up = FALSE, width = "500px",
-                    label = "Caption", tooltip = FALSE),
+                     p("A) The daily average percent of the ocean that experienced a MHW."),
+                     p("B) The proportion of the highest category of MHW experienced."),
+                     p("C) The total average of daily MHW occurrence throughout the entire ocean."),
+                     circle = FALSE,
+                     right = FALSE, up = FALSE, width = "500px",
+                     label = "Caption", tooltip = FALSE),
                    br(),
                    downloadButton(outputId = ns("historic_summary_dl"))
-                   )
+      )
     }
   })
   
-
+  
 # Figures -----------------------------------------------------------------
-
+  
   ### Load chosen year's summary; pre-rendered
   output$summary_all <- renderImage({
-      return(list(
-        src = paste0("../figures/MHW_cat_summary_",input$summary_year,".png"),
-        width = 1080,
-        height = 720,
-        contentType = "image/png"
-      ))
+    return(list(
+      src = paste0("../figures/MHW_cat_summary_",input$summary_year,".png"),
+      width = 1080,
+      height = 720,
+      contentType = "image/png"
+    ))
   }, deleteFile = FALSE)
   
   ### Load chosen year's summary; pre-rendered
@@ -67,7 +67,7 @@ summary <- function(input, output, session) {
       contentType = "image/png"
     ))
   }, deleteFile = FALSE)
-
+  
   
 # Downloads ---------------------------------------------------------------
   
--- shiny/modules/summary-ui.R
+++ shiny/modules/summary-ui.R
@@ -5,19 +5,21 @@ summaryUI <- function(id, label = 'summary') {
   
   # Side panel + main panel with tabs
   fluidPage(theme = "style.css",
-    sidebarLayout(fluid = T,
-                  uiOutput(outputId = ns("summary_UI")),
-                  mainPanel(width = 10,
-                            tabsetPanel(id = ns("summary_tabs"), #type = "pills",
-                                        tabPanel(title = "Annual", value = "annual_tab",
-                                                 shinycssloaders::withSpinner(imageOutput(outputId = ns("summary_all")), type = 6, color = "#b0b7be")
-                                                 ),
-                                        tabPanel(title = "Historic", value = "historic_tab",
-                                                 shinycssloaders::withSpinner(imageOutput(outputId = ns("historic_all")), type = 6, color = "#b0b7be"))
-                                        )
-                            )
-                  )
-    )
+            sidebarLayout(fluid = T,
+                          uiOutput(outputId = ns("summary_UI")),
+                          mainPanel(width = 10,
+                                    tabsetPanel(id = ns("summary_tabs"), #type = "pills",
+                                                tabPanel(title = "Annual", value = "annual_tab",
+                                                         shinycssloaders::withSpinner(imageOutput(outputId = ns("summary_all")), 
+                                                                                      type = 6, color = "#b0b7be")
+                                                ),
+                                                tabPanel(title = "Historic", value = "historic_tab",
+                                                         shinycssloaders::withSpinner(imageOutput(outputId = ns("historic_all")), 
+                                                                                      type = 6, color = "#b0b7be"))
+                                    )
+                          )
+            )
+  )
 }
 
 # cat("\nsummary_ui.R finished")
