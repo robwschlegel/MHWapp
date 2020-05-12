5d558ef1cba2584d1c62cfd1937d59f760690eed
robwschlegel <robwschlegel@gmail.com>
2020-05-12 10:17:35 -0300

    Going with a more agrressive rectangling approach

--- MHW_database.R
+++ MHW_database.R
@@ -11,10 +11,13 @@
 # 1: Setup ----------------------------------------------------------------
 
 # Libraries
+.libPaths(c("~/R-packages", .libPaths()))
 library(tidyverse)
 library(tidync)
-library(FNN)
-library(dtplyr) # Sometimes data.table can be faster
+library(doParallel)
+
+# Set cores
+registerDoParallel(cores = 25)
 
 # Metadata
 source("metadata/metadata.R")
@@ -40,34 +43,27 @@ source("metadata/metadata.R")
 # https://robwschlegel.github.io/heatwaveR/articles/gridded_event_detection.html
 
 # NCDUMP
-ncdump::NetCDF(CCI_files[1])$variable[1:5]
+# ncdump::NetCDF(CCI_files[1])$variable[1:5]
 
-# Regrid the CCI data to the OI grid
-system.time(
-CCI_regrid_dat <- left_join(CCI_dat, CCI_OISST_coords, by = c("lon", "lat")) %>% 
-  dplyr::select(lon_OI, lat_OI, analysed_sst) %>% 
-  group_by(lon_OI, lat_OI) %>% 
-  summarise(temp = mean(analysed_sst, na.rm = T)) %>% 
-  ungroup()
-) # 36 seconds
-
-# Visualise
-ggplot(data = CCI_regrid_dat, aes(x = lon_OI, y = lat_OI)) +
-  geom_tile(aes(fill = temp)) +
-  borders(colour = "black") +
-  scale_fill_viridis_c() +
-  coord_quickmap(expand = F) +
-  theme_void()
-
-# Function for loading the CCI pixels in a chosen OISST lon slice
+# Function for loading a day of CCI pixels in a chosen OISST lon slice
 # chosen_lon <- lon_OISST[1]
-# day_int <- 1
-CCI_OISST_lon_day <- function(day_int, chosen_lon){
+# lon_int <- 15
+# day_int <- 1318
+CCI_OISST_lon_day <- function(day_int, lon_int){
+  
+  # Set the range of lon values
+  lon_int_1 <- (lon_int*100)-99
+  lon_int_2 <- (lon_int*100)
+  if(lon_int_2 > 1440) lon_int_2 <- 1440
+  
+  # Find the range of lon values
+  lon_steps <- lon_OISST[seq(lon_int_1, lon_int_2)]
   
   # Find the pixels nearest to the chosen OISST lon slice
-  CCI_pixels <- filter(CCI_OISST_coords, lon_OI == chosen_lon)
+  CCI_pixels <- filter(CCI_OISST_coords, lon_OI %in% lon_steps)
   
-  # Extract and processes a CCI lon slice
+  # Extracts and processes a CCI lon slice
+  # system.time(
   CCI_lon_day <- tidync(CCI_files[day_int]) %>%
     hyper_filter(lon = dplyr::between(lon, min(CCI_pixels$lon), max(CCI_pixels$lon))) %>%
     hyper_tibble() %>%
@@ -80,7 +76,29 @@ CCI_OISST_lon_day <- function(day_int, chosen_lon){
     ungroup() %>% 
     mutate(t = as.Date(as.POSIXct(t, origin = '1981-01-01', tz = "GMT")),
            temp = round(temp-273.15, 2))
+  # )
   return(CCI_lon_day)
 }
-test <- CCI_OISST_lon_day(1,lon_OISST[1])
 
+# Function for loading all data within a lon slice
+# lon_int <- 1
+# registerDoParallel(cores = 50)
+CCI_OISST_lon_full <- function(lon_int){
+  date_int_range <- 1:length(seq(as.Date("1981-09-01"), as.Date("2018-12-31"), by = "day"))
+  print(paste0("Began run on ",lon_int," at ",Sys.time()))
+  CCI_lon_full <- plyr::ldply(date_int_range, CCI_OISST_lon_day, .parallel = T, 
+                              lon_int = lon_int) # ~45 minutes on 50 cores for 100 lon slices
+  lon_int_pad <- str_pad(lon_int, width = 2, pad = "0", side = "left")
+  saveRDS(CCI_lon_full, paste0("../data/CCI_lon/CCI_SST_",lon_int_pad,".Rds"))
+  rm(CCI_lon_full); gc()
+}
+
+# Run them all
+  # NB: Only run this to fully rectangle ALL of the CCI data
+  # This takes roughly 24 hours on 25 cores
+# registerDoParallel(cores = 25)
+plyr::l_ply(1:15, CCI_OISST_lon_full, .parallel = F, .progress = "text")
+
+# Calculate CCI MHWs based on the OISST clim period: 1982 - 2011
+
+# Create daily CCI MHW clim slice results
