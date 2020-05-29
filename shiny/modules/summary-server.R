4912f2f984bc145d41185df238a3092a39899476
robwschlegel <robwschlegel@gmail.com>
2020-05-29 13:52:38 -0300

    Converting the summay tab to a shinydashboard layout

--- shiny/modules/summary-server.R
+++ shiny/modules/summary-server.R
@@ -5,25 +5,31 @@ summary <- function(input, output, session) {
   
   # Render UI ---------------------------------------------------------------
   
+  # Radio buttons to choose clim period
+  summary_clim_period_radio <- prettyRadioButtons(inputId = ns("summary_clim_period"), label = "Clim. period:",
+                                                  choices = c("1982-2011", "1992-2018"),
+                                                  selected = "1992-2018", inline = F,
+                                                  status = "primary", fill = TRUE)
+  
   # Select years from a dropdown
-  picker_year <- pickerInput(inputId = ns("summary_year"), label = h4("Annual summary"),
-                             choices = seq(1982, lubridate::year(Sys.time())), multiple = FALSE,
-                             selected = lubridate::year(Sys.time())-1)
+  summary_year_picker <- pickerInput(inputId = ns("summary_year"), label = h4("Annual summary"),
+                                     choices = seq(1982, lubridate::year(Sys.time())), multiple = FALSE,
+                                     selected = lubridate::year(Sys.time())-1)
   
   # Select SST products from a dropdown
-  picker_product <- pickerInput(inputId = ns("summary_product"), label = h4("Product"),
-                               choices = c("OISST", "CCI", "CMC"), multiple = FALSE,
-                               selected = "OISST")
+  summary_product_picker <- pickerInput(inputId = ns("summary_product"), label = h4("Product"),
+                                        choices = c("OISST", "CCI", "CMC"), multiple = FALSE,
+                                        selected = "OISST")
   
   # The caption for the annual summary figure
   annual_caption <- dropdownButton(
     p("Annual global marine heatwave (MHW) occurrence."),
     p("A) Global map showing the highest category experienced at each pixel over the course of the year estimated using
-                       the NOAA OI v2 data set.White shows that no MHWs occurred in a pixel over the entire year."),
+      the NOAA OI v2 data set.White shows that no MHWs occurred in a pixel over the entire year."),
     p("B) Stacked barplot showing the percent of ocean pixels experiencing a MHW on any given day of the year."), 
     p("C) Stacked barplot showing the cumulative percent of the ocean that has experienced a MHW over the year. 
-                       These values are based on when in the year a pixel first experiences it's highest category MHW, so no pixel 
-                       is counted more than once. Horizontal bars in this figure show the final percentage values for each category of MHW."), 
+      These values are based on when in the year a pixel first experiences it's highest category MHW, so no pixel 
+      is counted more than once. Horizontal bars in this figure show the final percentage values for each category of MHW."), 
     p("D) Stacked barplots showing the cumulative number of MHW days averaged over all pixels in the ocean."),
     circle = FALSE,
     right = FALSE, up = FALSE, width = "500px",
@@ -48,18 +54,96 @@ summary <- function(input, output, session) {
   
   # The chosen controls per tab
   output$summarySidebarControls <- renderUI({
-    req(input$summaryMenu)
-    if(input$summaryMenu == "annual"){
-      sidebarMenu(picker_product, picker_year, annual_caption, download_annual_summary)
-    } else if(input$summaryMenu == "total"){
-      sidebarMenu(picker_product, total_caption, download_total_summary)
+    if(input$summaryMenu == "summary_annual"){
+      sidebarMenu(summary_clim_period_radio, summary_product_picker, summary_year_picker, annual_caption, download_annual_summary)
+    } else if(input$summaryMenu == "summary_total"){
+      sidebarMenu(summary_clim_period_radio, summary_product_picker, total_caption, download_total_summary)
     } else {
       # Intentionally empty
     }
   })
   
+  # Reactive values ---------------------------------------------------------
+  
+  ## Values used to inform the pickers as to the valid options based on clim period + product
+  
+  ## The pixel files based on reactive product, year, and clim period choices
+  cat_pixel <- reactive({
+    req(input$summary_year); req(input$summary_product); req(input$summary_clim_period)
+    file_name <- paste0("../data/annual_summary/",input$summary_product,"_cat_pixel_",
+                        input$summary_clim_period,"_",input$summary_year,".Rds")
+    if(file.exists(file_name)) cat_pixel <- readRDS(file_name)
+  })
+
+  ## The daily files based on reactive years and clim period choices
+  cat_daily <- reactive({
+    req(input$summary_year); req(input$summary_product); req(input$summary_clim_period)
+    file_name <- paste0("../data/annual_summary/",input$summary_product,"_cat_daily_",
+                        input$summary_clim_period,"_",input$summary_year,".Rds")
+    if(file.exists(file_name)){
+      cat_daily <- readRDS(file_name)
+      return(cat_daily)
+    }
+  })
+  
+  ## The total summary file
+  cat_total <- reactive({
+    req(input$summary_product); req(input$summary_clim_period)
+    file_name <- paste0("../data/annual_summary/",input$summary_product,"_cat_daily_",
+                        input$summary_clim_period,"_total.Rds")
+    if(file.exists(file_name)) cat_total <- readRDS(file_name)
+    return(cat_total)
+  })
+  
   
   # Figures -----------------------------------------------------------------
+  
+  ## The total display
+  ### Stacked barplot of global daily count of MHWs by category
+  output$total_daily_count <- renderPlot({
+    
+    cat_total <- cat_total()
+    
+    ggplot(cat_total, aes(x = t, y = cat_prop_daily_mean)) +
+      geom_bar(aes(fill = category), stat = "identity", show.legend = T,
+               position = position_stack(reverse = TRUE), width = 1) +
+      scale_fill_manual("Category", values = MHW_colours) +
+      scale_y_continuous(limits = c(0, 1),
+                         breaks = seq(0.2, 0.8, length.out = 4),
+                         labels = paste0(seq(20, 80, by = 20), "%")) +
+      scale_x_continuous(breaks = seq(1982, 2019, 5)) +
+      labs(y = "Daily MHW occurrence", x = NULL) +
+      coord_cartesian(expand = F) +
+      guides(fill = guide_legend(nrow = 2, byrow = TRUE)) +
+      theme(axis.title = element_text(size = 14),
+            axis.text = element_text(size = 12),
+            legend.position = "bottom",
+            legend.title = element_text(size = 18),
+            legend.text = element_text(size = 16))
+  })
+  
+  ### Stacked barplot of cumulative percent of ocean affected by MHWs
+  output$total_cum_perc <- renderPlot({
+    
+    cat_total <- cat_total()
+    
+    ggplot(cat_total, aes(x = t, y = first_n_cum_prop)) +
+      geom_bar(aes(fill = category), stat = "identity", show.legend = T,
+               position = position_stack(reverse = TRUE), width = 1) +
+      scale_fill_manual("Category", values = MHW_colours) +
+      scale_y_continuous(limits = c(0, 1),
+                         breaks = seq(0.2, 0.8, length.out = 4),
+                         labels = paste0(seq(20, 80, by = 20), "%")) +
+      scale_x_continuous(breaks = seq(1982, 2019, 5)) +
+      labs(y = "Total MHW occurrence", x = NULL) +
+      coord_cartesian(expand = F) +
+      guides(fill = guide_legend(nrow = 2, byrow = TRUE)) +
+      theme(axis.title = element_text(size = 14),
+            axis.text = element_text(size = 12),
+            legend.position = "bottom",
+            legend.title = element_text(size = 18),
+            legend.text = element_text(size = 16))
+  })
   
   ### Load chosen year's summary; pre-rendered
   output$summary_all <- renderImage({
@@ -81,6 +165,12 @@ summary <- function(input, output, session) {
     ))
   }, deleteFile = FALSE)
   
+  
+  # Test box
+  output$test_box <- renderText({
+    paste0(input$summary_year, input$summary_product, input$summary_clim_period)
+    })
+
   
   # Downloads ---------------------------------------------------------------
   
--- shiny/modules/summary-ui.R
+++ shiny/modules/summary-ui.R
@@ -1,78 +1,55 @@
 summaryUI <- function(id, label = 'summary') {
   ns <- NS(id)
   
-  # Side panel + main panel with tabs
-  fluidPage(theme = "style.css",
-            sidebarLayout(fluid = T,
-                          uiOutput(outputId = ns("summary_UI")),
-                          mainPanel(width = 10,
-                                    tabsetPanel(id = ns("summary_tabs"), #type = "pills",
-                                                tabPanel(title = "Annual", value = "annual_tab",
-                                                         shinycssloaders::withSpinner(imageOutput(outputId = ns("summary_all")),
-                                                                                      type = 6, color = "#b0b7be")
-                                                ),
-                                                tabPanel(title = "Historic", value = "historic_tab",
-                                                         shinycssloaders::withSpinner(imageOutput(outputId = ns("historic_all")),
-                                                                                      type = 6, color = "#b0b7be"))
-                                    )
-                          )
-            )
-  )
-}
-
-# cat("\nsummary_ui.R finished")
-
-summaryUI <- function(id, label = 'summary') {
-  ns <- NS(id)
-
-  ## Tracker ideas
-  # - Show comparisons in two panels next to each other
-  # - Preferably a dashboard
-  # - High level choice on the left is which year is being compared
-  # - This will also allow for an overall comparison and seasons (e.g, Q1 - Q4)
-
   dashboardPage(skin = "black",
-
+                
                 # The app title
                 dashboardHeader(disable = TRUE),
-
+                
                 # The primary options
                 dashboardSidebar(
                   sidebarMenu(id = ns("summaryMenu"),
-                              menuItem("Annual", tabName = "annual", icon = icon("chart-bar"), selected = TRUE),
-                              menuItem("Total", tabName = "total", icon = icon("chart-bar")),
-                              # menuItem("Map", tabName = "map", icon = icon("map")),
-                              # menuItem("Season", tabName = "season", icon = icon("chart-pie")),
-                              # menuItem("Tables", tabname = "tables", icon = icon("table")),
-                              menuItem("About", tabName = "about", icon = icon("question")),
+                              menuItem("Total", tabName = "summary_total", icon = icon("chart-bar")),
+                              # menuItem("Annual", tabName = "summary_annual", icon = icon("chart-bar")),
+                              menuItem("About", tabName = "summary_about", icon = icon("question"), selected = TRUE),
                               # The reactive controls based on the primary option chosen
-                              uiOutput(outputId = ns("summarySidebarControls")))
+                              uiOutput(outputId = ns("summarySidebarControls"))
+                              )
                 ),
-
+                
                 # The dashboard
                 dashboardBody(
                   tabItems(
-
-
+                    
                     # Annual figures ----------------------------------------------------------
-
-
-                    tabItem(tabName = "annual",
-                            fluidRow(box(imageOutput(outputId = ns("summary_all"), type = 6, color = "#b0b7be")), 
-                                     width = 12, title = "Annual summary", status = "primary", solidHeader = TRUE, collapsible = TRUE)),
-
-
+                    
+                    # tabItem(tabName = "summary_annual",
+                    #         fluidRow(box(imageOutput(outputId = ns("summary_all")), 
+                    #                      width = 12, title = "Annual summary", status = "primary", solidHeader = TRUE, collapsible = TRUE))
+                    #         ),
+                    
+                    
                     # Total figures -----------------------------------------------------------
-
-                  tabItem(tabName = "total",
-                          fluidRow(box(shinycssloaders::withSpinner(imageOutput(outputId = ns("total_all")),
-                                                                    type = 6, color = "#b0b7be")), width = 12, title = "Total summary",
-                                   status = "primary", solidHeader = TRUE, collapsible = TRUE)),
-
-
+                    
+                    tabItem(tabName = "summary_total",
+                            # fluidRow(box(imageOutput(outputId = ns("total_all")), 
+                                         # width = 12, title = "Total summary", status = "primary", solidHeader = TRUE, collapsible = TRUE))
+                            fluidRow(box(plotOutput(outputId = ns("total_daily_count")), width = 4, 
+                                         title = "Average daily MHW occurrence per year (% of ocean)", 
+                                         status = "primary", solidHeader = TRUE, collapsible = TRUE),
+                                     box(plotOutput(outputId = ns("total_cum_perc")), width = 4, 
+                                         title = "Total coverage per year (% of ocean)", 
+                                         status = "warning", solidHeader = TRUE, collapsible = TRUE),
+                                     box(plotlyOutput(ns("total_Cum")), width = 12, 
+                                         title = "Total MHW days per pixel per year",
+                                         status = "success", solidHeader = TRUE, collapsible = TRUE))#,
+                            # fluidRow(box(textOutput(outputId = ns("test_box"))))
+                            ),
+                    
+                    
                     # App explanation ---------------------------------------------------------
-
-                    tabItem(tabName = "about",
+                    
+                    tabItem(tabName = "summary_about", 
                             h2("This is where the functioning of the app will be explained..."))
                   )
                 )
