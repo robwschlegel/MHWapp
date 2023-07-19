adc0053272e80af1a8a9d3ce4cd4f6747950d2cc
robwschlegel <robwschlegel@gmail.com>
2023-07-19 08:07:17 -0300

    Moved legacy UI and server to the map module

--- shiny/modules/map-server.R
+++ shiny/modules/map-server.R
@@ -1383,3 +1383,23 @@ map <- function(input, output, session) {
 
 # cat("\nmap_server.R finished")
 
+
+# Legacy server -----------------------------------------------------------
+
+# Legacy code for the main server for v1.0
+# server <- function(input, output, session){
+#   
+#   shinyjs::hide(id = "loading-content", anim = TRUE, animType = "fade", time = 2)
+#   
+#   callModule(map, "map")
+#   
+#   # Deactivated primarily for speed, but also because the underlying packages change too often
+#   # callModule(comparison, "comparison")
+#   # callModule(summary, "summary")
+#   
+#   callModule(about, 'about')
+#   
+#   # During testing...
+#   # session$onSessionEnded(stopApp)
+# }
+
--- shiny/modules/map-ui.R
+++ shiny/modules/map-ui.R
@@ -87,3 +87,40 @@ mapUI <- function(id, label = 'map') {
 
 #cat("\nmap_ui.R finished")
 
+
+# Legacy UI ---------------------------------------------------------------
+
+# Legacy code for the main UI for v1.0
+# ui <- tagList(shinyjs::useShinyjs(),  
+#               # CSS
+#               tags$head(includeCSS("style.css")),
+#               # Loading message
+#               div(id = "loading-content",
+#                   br(), br(),
+#                   h2("Loading"),
+#                   br(), br(), br(),
+#                   h5("If this screen lasts for more than 10 seconds please try Firefox."),
+#                   br(),
+#                   h5("If Firefox doesn't work you may be experiencing a Mac OS Javascript bug."),
+#                   br(),
+#                   h5("Please consult the 'Bugs?' section on the 'About' page for more information.")),
+#               # Nav bar tabs
+#               navbarPage(title = "Marine Heatwave Tracker",
+#                          selected = 'map_tab',
+#                          header = list(cicerone::use_cicerone()), # Start guided tour
+#                          tabPanel(title = 'Map',
+#                                   value = 'map_tab',
+#                                   mapUI('map')),
+#                          # Deactivated primarily for speed, but also because the underlying packages change too often
+#                          # tabPanel(title = 'Comparison',
+#                          #          value = 'comp_tab',
+#                          #          comparisonUI('comparison')),
+#                          # tabPanel(title = 'Summary',
+#                          #          value = 'sum_tab',
+#                          #          summaryUI('summary')),
+#                          tabPanel(title = 'About',
+#                                   value = 'about_tab',
+#                                   aboutUI('about')),
+#                          collapsible = TRUE, fluid = TRUE)
+# )
+
